f9d1a6ab90a8ca0282373e50519cfac10727d337
Wanten <WantenMN@gmail.com>
2023-06-15 13:06:23 +0800

    chore: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
-import { Footer, Header, Main } from "./layouts";
 
 const App = () => {
   return (
     <div className="min-h-screen">
-      <Header />
-
-      <Main>
-        <Outlet />
-      </Main>
-      <Footer />
+      <Outlet />
 
       <ScrollRestoration />
     </div>
--- src/layouts/Footer/Footer.tsx
+++
@@ -1,4 +0,0 @@
-const Footer = () => {
-  return <footer className="h-20 border-t">Footer</footer>;
-};
-export default Footer;
