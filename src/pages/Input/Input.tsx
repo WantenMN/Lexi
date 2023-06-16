e7cc4c841d1afd7cd16d0b5af54b07b5c54be735
Wanten <WantenMN@gmail.com>
2023-06-16 10:05:35 +0800

    feat(input): add save/read article functionality

--- src/pages/Input/Input.tsx
+++ src/pages/Input/Input.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { getBase, setBase } from "@/utils/idb";
 
 const Input = () => {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -9,13 +10,38 @@ const Input = () => {
     }
   };
 
+  const saveArticle = async () => {
+    if (!textareaRef.current) return;
+
+    const article = textareaRef.current.value;
+    await setBase("article", article);
+  };
+
+  const readArticle = async () => {
+    const article = await getBase("article");
+    if (!textareaRef.current) return;
+
+    textareaRef.current.value = article;
+  };
+
+  let timer: NodeJS.Timeout;
+  const handleTextareaChange = () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      saveArticle();
+    }, 250);
+  };
+
   useEffect(() => {
+    readArticle();
     textareaGetFocus();
   }, []);
 
   return (
     <textarea
       ref={textareaRef}
+      onChange={handleTextareaChange}
+      onPaste={saveArticle}
       className="h-full w-full resize-none bg-inherit p-4 text-lg leading-normal focus:outline-none "
       placeholder="Please paste article here"
     ></textarea>
