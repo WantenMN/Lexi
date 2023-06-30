b135d66521da5c6940949102a9d844a05f8d1d7a
Wanten <WantenMN@gmail.com>
2023-06-30 09:01:45 +0800

    refactor(App): update import name of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Container from "./components/Container/Container";
-import NavBar from "./components/Nav/Nav";
+import Nav from "./components/Nav/Nav";
 import DisplayArea from "./components/DisplayArea/DisplayArea";
 
 const App = () => {
   return (
     <div className="to flex min-h-screen items-center justify-center bg-gradient-to-br from-pink-100 via-blue-100 to-red-100">
       <Container>
-        <NavBar />
+        <Nav />
         <DisplayArea>
           <Outlet />
         </DisplayArea>
