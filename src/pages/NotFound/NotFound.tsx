335ebab595d9bd0da75a1d163de1c39b08caf46d
Wanten <WantenMN@gmail.com>
2023-06-30 08:42:33 +0800

    style: format code

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="flex h-0 w-full items-center justify-center">
-      <div className="mt-96 text-4xl font-bold sm:text-6xl">404 Not Found</div>
+      <div className=" mt-96 text-4xl font-bold sm:text-6xl">404 Not Found</div>
     </div>
   );
 };
